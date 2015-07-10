d75370caa4931a4f6f876ffd23396252c221934f
Christopher Brown <io@henrian.com>
2015-07-10 10:26:43 -0500

    Basic implementation with simplistic format/pattern matching

---
+++ README.md
@@ -0,0 +1,29 @@
+# randomized
+
+Randomize string input, e.g., for documentation examples.
+
+    npm install -g randomized
+
+Use command line arguments:
+
+    randomized 205.52.5.53
+    > 14.141.14.129
+
+    randomized 181df46d-b7b8-6dca-b750-d93304c99413
+    > a5120c5a-2a5c-7f1f-ba21-9302f9599027
+
+Or `/dev/stdin`:
+
+    echo 3F5MSXGS5C74TCRNGI8I78WJPL78O6 | randomized
+    > 8SOWM4XECCO66FO5P5EVZYWJAPWPXM
+
+Or as a module:
+
+    var randomized = require('randomized');
+    var output = randomized.convert('wTUaxO87fR1a');
+    console.log(output);
+
+
+## License
+
+Copyright 2015 Christopher Brown. [MIT Licensed](http://chbrown.github.io/licenses/MIT/#2015).
---
+++ index.js
@@ -0,0 +1,109 @@
+var DIGITS = '0123456789';
+var HEXA_UPPER = '0123456789ABCDEF';
+var HEXA_LOWER = '0123456789abcdef';
+var ALPHA_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var ALPHA_LOWER = 'abcdefghijklmnopqrstuvwxyz';
+var ALPHA_MIXED = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+var ALPHANUMERIC_UPPER = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var ALPHANUMERIC_LOWER = '0123456789abcdefghijklmnopqrstuvwxyz';
+var ALPHANUMERIC_MIXED = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+function sample(population, length) {
+    var choices = [];
+    for (var i = 0; i < length; i++) {
+        choices.push(population[(Math.random() * population.length) | 0]);
+    }
+    return choices.join('');
+}
+function randomInteger(min, max) {
+    return ((Math.random() * (max - min)) + min) | 0;
+}
+function uuid(pop) {
+    return sample(pop, 8) + "-" + sample(pop, 4) + "-" + sample(pop, 4) + "-" + sample(pop, 4) + "-" + sample(pop, 12);
+}
+var predefined_formats = [
+    {
+        // uppercase uuid
+        regExp: /^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/,
+        generate: function (input) { return uuid(HEXA_UPPER); }
+    },
+    {
+        // lowercase uuid
+        regExp: /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
+        generate: function (input) { return uuid(HEXA_LOWER); }
+    },
+    {
+        // IPv4
+        regExp: /^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/,
+        generate: function (input) {
+            return randomInteger(0, 256) + "." + randomInteger(0, 256) + "." + randomInteger(0, 256) + "." + randomInteger(0, 256);
+        }
+    },
+    {
+        // general digits-only
+        regExp: /^[0-9]+$/,
+        generate: function (input) { return sample(DIGITS, input.length); }
+    },
+    {
+        // general uppercase hexadecimal
+        regExp: /^[0-9A-F]+$/,
+        generate: function (input) { return sample(HEXA_UPPER, input.length); }
+    },
+    {
+        // general lowercase hexadecimal
+        regExp: /^[0-9a-f]+$/,
+        generate: function (input) { return sample(HEXA_LOWER, input.length); }
+    },
+    {
+        // general uppercase alphabetic
+        regExp: /^[A-Z]+$/,
+        generate: function (input) { return sample(ALPHA_UPPER, input.length); }
+    },
+    {
+        // general lowercase alphabetic
+        regExp: /^[a-z]+$/,
+        generate: function (input) { return sample(ALPHA_LOWER, input.length); }
+    },
+    {
+        // general mixedcase alphabetic
+        regExp: /^[A-Za-z]+$/,
+        generate: function (input) { return sample(ALPHA_MIXED, input.length); }
+    },
+    {
+        // general uppercase alphanumeric
+        regExp: /^[0-9A-Z]+$/,
+        generate: function (input) { return sample(ALPHANUMERIC_UPPER, input.length); }
+    },
+    {
+        // general lowercase alphanumeric
+        regExp: /^[0-9a-z]+$/,
+        generate: function (input) { return sample(ALPHANUMERIC_LOWER, input.length); }
+    },
+    {
+        // general mixedcase alphanumeric
+        regExp: /^[0-9A-Za-z]+$/,
+        generate: function (input) { return sample(ALPHANUMERIC_MIXED, input.length); }
+    },
+];
+function convert(input) {
+    for (var i = 0, format; (format = predefined_formats[i]); i++) {
+        if (format.regExp.test(input)) {
+            return format.generate(input);
+        }
+    }
+    throw new Error("Unable to recognize format of input: " + input);
+}
+exports.convert = convert;
+function readStream(stream, callback) {
+    var chunks = [];
+    stream
+        .on('error', function (error) {
+        callback(error);
+    })
+        .on('data', function (chunk) {
+        chunks.push(chunk);
+    })
+        .on('end', function () {
+        callback(null, chunks.join(''));
+    });
+}
+exports.readStream = readStream;
